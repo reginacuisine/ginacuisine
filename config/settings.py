2e6b78558ca64dafef6718e8a71128b5ee3c75a9
reginacuisine <reginatonde44@gmail.com>
2026-02-06 00:00:21 +0000

    Création automatique du superuser

--- config/settings.py
+++ config/settings.py
@@ -94,3 +94,10 @@ EMAIL_PORT = 587
 EMAIL_HOST = 'smtp.gmail.com'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# À SUPPRIMER APRÈS LE PREMIER DÉPLOIEMENT RÉUSSI
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('admin', 'reginatonde44@gmail.com', '79603216tonde')
+
